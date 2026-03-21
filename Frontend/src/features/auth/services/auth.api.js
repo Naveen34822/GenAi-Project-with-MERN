c196544dbcabf8ca68be3758a05e52acc5ac4671
Naveen <naveenkalera4523@gmail.com>
2026-03-21 13:45:09 +0530

    update base url to production backend

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://genai-project-with-mern.onrender.com",
   withCredentials: true  //to send cookies with every request
 })
 
